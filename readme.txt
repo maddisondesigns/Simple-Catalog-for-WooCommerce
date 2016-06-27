74c6fa23e51786e5071c21314256ecb1a3798378
Anthony Hortin <anthony@maddisondesigns.com>
2016-06-28 00:44:10 +1000

    Initial commit. Yay!

---
+++ readme.txt
@@ -0,0 +1,60 @@
+=== Simple Catalog for WooCommerce ===
+Contributors: ahortin
+Donate Link: http://maddisondesigns.com/simple-catalog-for-woocommerce
+Tags: ecommerce, e-commerce, commerce, woothemes, wordpress ecommerce, woocommerce
+Requires at least: 4.5
+Tested up to: 4.5.3
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Turn your WooCommerce store into a simple online catalog or alternatively, only allow store access to customers once they're logged in.
+
+== Description ==
+
+Simple Catalog for WooCommerce turns your WooCommerce store into a simple online catalog site.
+
+WooCommerce is a fantastic eCommerce plugin, but sometimes you might only want to showcase your products rather than sell them. This plugin allows you to use the power of WooCommerce to manage all your products whilst hiding the eCommerce functionality like the Add to Cart Buttons, Prices, Ratings and the Cart & Checkout pages from your customers.
+
+Later on, when you decide you're ready to sell your products online, simply disable this plugin and all your WooCommerce functionality will return.
+
+Using this plugin you're able to restrict the eCommerce functionality to all your customers, or just to customers who haven't logged in.
+
+When hiding your prices, product ratings and Add to Cart buttons, you're able to hide them completely or just hide them from users who haven't logged in. You also have the option of showing a message to your customers in place of your price.
+
+For use with WooCommerce 2.6 and above.
+
+
+== Installation ==
+
+1. Upload the 'simple-catalog-for-woocommerce' folder to your '/wp-content/plugins/' directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Go to the 'Settings > WC Breadcrumbs' menu to update the plugin settings
+
+
+== Frequently Asked Questions ==
+
+= What version of WooCommerce does this work for? =
+This plugin has been tested with WooCommerce 2.6+.
+
+= How do I get my eCommerce functionality back? =
+That's easy! Simply deactivate this plugin and all your WooCommerce functionality will be returned.
+
+
+== Screenshots ==
+
+1. Simple Catalog for WooCommerce settings
+2. Product Price hidden on Shop page and optional text displayed
+3. Product Price hidden on Single Product page and optional text displayed
+4. Product Price & Ratings hidden on Shop page
+5. Product Price & Ratings hidden on Single Product page
+6. Simple Catalog for WooCommerce settings
+
+
+== Changelog ==
+
+= 1.0 =
+- Initial version. Yay!
+
+
+== Upgrade Notice ==
---
+++ simple-catalog-for-woocommerce.php
@@ -0,0 +1,333 @@
+<?php
+/*
+Plugin Name: Simple Catalog for WooCommerce
+Plugin URI: http://maddisondesigns.com/simple-catalog-for-woocommerce
+Description: Turn your WooCommerce store into a simple online catalog. You can disable your eCommerce functionality for all users or only for users that aren't logged in.
+Version: 1.0
+Author: Anthony Hortin
+Author URI: http://maddisondesigns.com
+Text Domain: simple-woocommerce-catalog
+License: GPLv2
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, see http://www.gnu.org/licenses/gpl-2.0.html.
+*/
+
+
+class scw_simple_catalog_woocommerce_plugin {
+
+	const SETTINGS_NAMESPACE = 'simple_woocommerce_catalog';
+
+	public function __construct() {
+		if ( is_admin() ) {
+			add_filter( 'woocommerce_settings_tabs_array', array( $this, 'scw_add_settings_tab' ), 50) ;
+			add_action( 'woocommerce_settings_tabs_' . self::SETTINGS_NAMESPACE, array( $this, 'scw_catalog_settings_tab' ) );
+			add_action( 'woocommerce_update_options_' . self::SETTINGS_NAMESPACE, array( $this, 'scw_catalog_update_settings' ) );
+			add_filter( 'plugin_action_links', array( $this, 'scw_add_settings_link'), 10, 2);
+		}
+		else {
+			add_action( 'init', array( $this, 'scw_catalog_remove_cart_buttons' ) );
+			add_action( 'init', array( $this, 'scw_catalog_remove_prices' ) );
+			add_action( 'init', array( $this, 'scw_catalog_remove_ratings' ) );
+			add_action('template_redirect', array( $this, 'scw_catalog_cart_redirect') );
+			add_action('template_redirect', array( $this, 'scw_catalog_checkout_redirect') );
+		}
+	}
+
+	/**
+	 * Add a new tab to the WooCommerce Settings page
+	 */
+	public static function scw_add_settings_tab( $settings_tabs ) {
+		$settings_tabs[self::SETTINGS_NAMESPACE] = __( 'Simple Catalog', 'simple-woocommerce-catalog' );
+		return $settings_tabs;
+	}
+
+	/**
+	 * Add a settings link to plugin page
+	 */
+	public function scw_add_settings_link( $links, $file ) {
+		static $this_plugin;
+
+		if( !$this_plugin ) {
+			$this_plugin = plugin_basename( __FILE__ );
+		}
+
+		if( $file == $this_plugin ) {
+			$settings_link = '<a href="admin.php?page=wc-settings&tab=' . self::SETTINGS_NAMESPACE . '">' . __( 'Settings', 'simple-woocommerce-catalog' ) . '</a>';
+			array_unshift( $links, $settings_link ) ;
+		}
+
+		return $links;
+	}
+
+	/**
+	 * Get the settings for our WooCommerce tab
+	 */
+	public function scw_catalog_settings_tab() {
+		woocommerce_admin_fields( $this->scw_get_tab_settings() );
+	}
+
+	/**
+	 * Update the settings for our WooCommerce tab
+	 */
+	public function scw_catalog_update_settings() {
+		woocommerce_update_options( $this->scw_get_tab_settings() );
+	}
+
+	/**
+	* Get an option set in our settings tab
+	*/
+	public function scw_catalog_get_option( $key ) {
+		$fields = $this->scw_get_tab_settings();
+
+		return apply_filters( 'wc_option_' . $key, get_option( 'wc_settings_' . self::SETTINGS_NAMESPACE . '_' . $key, ( ( isset( $fields[$key] ) && isset( $fields[$key]['default'] ) ) ? $fields[$key]['default'] : '' ) ) );
+	}
+
+	/**
+	 * Add all our settings to our WooCommerce tab
+	 */
+	private function scw_get_tab_settings() {
+		$settings = array(
+			'section_title' => array(
+				'name'     => __( 'Catalog Options', 'simple-woocommerce-catalog' ),
+				'type'     => 'title',
+				'id'       => 'wc_settings_' . self::SETTINGS_NAMESPACE . '_section_title'
+			),
+			'addtocart' => array(
+				'name' => __( 'Add to Cart Button', 'simple-woocommerce-catalog' ),
+				'desc_tip' => __( 'Hide the Add to Cart Button for all customers or just Logged In customers.', 'simple-woocommerce-catalog' ),
+				'type' => 'select',
+				'default' => 'loggedin',
+				'options' => array(
+					'display' => 'Display for all Users',
+					'hide' => 'Hide for all Users',
+					'loggedin' => 'Display for Logged In Users'
+				),
+				'id'   => 'wc_settings_' . self::SETTINGS_NAMESPACE . '_addtocart'
+			),
+			'showprices' => array(
+				'name' => __( 'Prices', 'simple-woocommerce-catalog' ),
+				'desc_tip' => __( 'Hide the prices for all customers or just Logged In customers.', 'simple-woocommerce-catalog' ),
+				'type' => 'select',
+				'default' => 'loggedin',
+				'options' => array(
+					'display' => 'Display for all Users',
+					'hide' => 'Hide for all Users',
+					'loggedin' => 'Display for Logged In Users'
+				),
+				'id'   => 'wc_settings_' . self::SETTINGS_NAMESPACE . '_showprices'
+			),
+			'showratings' => array(
+				'name' => __( 'Product Ratings', 'simple-woocommerce-catalog' ),
+				'desc_tip' => __( 'Hide the product ratings for all customers or just Logged In customers.', 'simple-woocommerce-catalog' ),
+				'type' => 'select',
+				'default' => 'loggedin',
+				'options' => array(
+					'display' => 'Display for all Users',
+					'hide' => 'Hide for all Users',
+					'loggedin' => 'Display for Logged In Users'
+				),
+				'id'   => 'wc_settings_' . self::SETTINGS_NAMESPACE . '_showratings'
+			),
+			'pricetext' => array(
+				'name' => __( 'Price Text Replacement', 'simple-woocommerce-catalog' ),
+				'desc_tip' => __( 'Display this text instead of the price. Only displayed when price is hidden from customers.', 'simple-woocommerce-catalog' ),
+				'type' => 'text',
+				'default' => '',
+				'id'   => 'wc_settings_' . self::SETTINGS_NAMESPACE . '_pricetext'
+			),
+			'hidecart' => array(
+				'name' => __( 'Cart Page', 'simple-woocommerce-catalog' ),
+				'desc_tip' => __( 'Hide the Cart page from all customers or just Logged In customers.', 'simple-woocommerce-catalog' ),
+				'type' => 'select',
+				'default' => 'display',
+				'options' => array(
+					'display' => 'Display for all Users',
+					'hide' => 'Hide for all Users',
+					'loggedin' => 'Display for Logged In Users'
+				),
+				'id'   => 'wc_settings_' . self::SETTINGS_NAMESPACE . '_hidecart'
+			),
+			'hidecheckout' => array(
+				'name' => __( 'Checkout Page', 'simple-woocommerce-catalog' ),
+				'desc_tip' => __( 'Hide the Checkout page from all customers or just Logged In customers.', 'simple-woocommerce-catalog' ),
+				'type' => 'select',
+				'default' => 'display',
+				'options' => array(
+					'display' => 'Display for all Users',
+					'hide' => 'Hide for all Users',
+					'loggedin' => 'Display for Logged In Users'
+				),
+				'id'   => 'wc_settings_' . self::SETTINGS_NAMESPACE . '_hidecheckout'
+			),
+			'section_end' => array(
+				'type' => 'sectionend',
+				'id' => 'wc_settings_' . self::SETTINGS_NAMESPACE . '_section_end'
+			)
+		);
+		return apply_filters( 'wc_settings_tab_' . self::SETTINGS_NAMESPACE, $settings );
+	}
+
+	/**
+	 * Check what we should do with the Add to Cart button
+	 */
+	public function scw_catalog_remove_cart_buttons() {
+		switch ( $this->scw_catalog_get_option( 'addtocart' ) ) {
+			case 'hide':
+				$this->scw_hide_cart_buttons();
+				break;
+
+			case 'loggedin':
+				if ( !is_user_logged_in() ) {
+					$this->scw_hide_cart_buttons();
+				}
+				break;
+		}
+	}
+
+	/**
+	 * Hide the Add to Cart button on the shop page and the single product page
+	 */
+	public function scw_hide_cart_buttons() {
+		remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart');
+		remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 30 );
+	}
+
+	/**
+	 * Check what we should do with the product prices
+	 */
+	public function scw_catalog_remove_prices() {
+		switch ( $this->scw_catalog_get_option( 'showprices' ) ) {
+			case 'hide':
+				$this->scw_hide_product_prices();
+				break;
+
+			case 'loggedin':
+				if ( !is_user_logged_in() ) {
+					$this->scw_hide_product_prices();
+				}
+				break;
+		}
+	}
+
+	/**
+	 * Hide the product price on the shop page and the single product page. Change the price text, if needed.
+	 */
+	public function scw_hide_product_prices() {
+		remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price' );
+		remove_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_price' );
+		if ( !empty( $this->scw_catalog_get_option( 'pricetext' ) ) ) {
+			add_action( 'woocommerce_single_product_summary', array( $this, 'scw_catalog_price_text_replacement' ), 11 );
+			add_action( 'woocommerce_after_shop_loop_item_title', array( $this, 'scw_catalog_price_text_replacement' ), 11 );
+		}
+	}
+
+	/**
+	 * Change the price text when the prices are hidden from customers
+	 */
+	public function scw_catalog_price_text_replacement() {
+		echo '<p class="price">' . $this->scw_catalog_get_option( 'pricetext' ) . '</p>';
+	}
+
+	/**
+	 * Check what we should do with the product ratings
+	 */
+	public function scw_catalog_remove_ratings() {
+		switch ( $this->scw_catalog_get_option( 'showratings' ) ) {
+			case 'hide':
+				$this->scw_hide_product_ratings();
+				break;
+
+			case 'loggedin':
+				if ( !is_user_logged_in() ) {
+					$this->scw_hide_product_ratings();
+				}
+				break;
+		}
+	}
+
+	/**
+	 * Hide the product ratings on the shop page and the single product page.
+	 */
+	public function scw_hide_product_ratings() {
+		remove_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_rating', 5 );
+		remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_rating', 10);
+	}
+
+	/**
+	 * Check if we should be hiding the cart page
+	 */
+	public function scw_catalog_cart_redirect() {
+		switch ( $this->scw_catalog_get_option( 'hidecart' ) ) {
+			case 'hide':
+				if ( is_cart() ) {
+					wp_redirect( home_url() );
+					exit;
+				}
+				break;
+
+			case 'loggedin':
+				if ( !is_user_logged_in() && is_cart() ) {
+					wp_redirect( home_url() );
+					exit;
+				}
+				break;
+
+			default:
+				return;
+				break;
+		}
+	}
+
+	/**
+	 * Check if we should be hiding the checkout page
+	 */
+	public function scw_catalog_checkout_redirect() {
+		switch ( $this->scw_catalog_get_option( 'hidecheckout' ) ) {
+			case 'hide':
+				if ( is_checkout() ) {
+					wp_redirect( home_url() );
+					exit;
+				}
+				break;
+
+			case 'loggedin':
+				if ( !is_user_logged_in() && is_checkout() ) {
+					wp_redirect( home_url() );
+					exit;
+				}
+				break;
+
+			default:
+				return;
+				break;
+		}
+	}
+}
+
+if ( scw_is_woocommerce_active() ) {
+	$scw_simple_catalog_woocommerce = new scw_simple_catalog_woocommerce_plugin();
+}
+
+/**
+ * Check if WooCommerce is active
+ */
+function scw_is_woocommerce_active() {
+	if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
